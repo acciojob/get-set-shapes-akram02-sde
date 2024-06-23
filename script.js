b44a266b67f590bb83cafb5598d58ea9800d26f2
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-23 13:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this.width = width;
+		this.height = height;
+	}
 
-class Square extends Animal {}
+	getWidth() {
+		return this.width;
+	}
+
+	getHeight() {
+		return this.height;
+	}
+
+	getArea() {
+		return this.width * this.height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+		super(side, side)
+	}
+
+	getPerimeter() {
+		return this.side * 4;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
